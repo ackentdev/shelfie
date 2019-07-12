2f65d0a4f6aa5a90b40330c5c823a056add96ec7
Adam Kent <Adam@Adams-MacBook-Pro-2.local>
2019-07-12 12:32:38 -0700

    added controller for .get

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const massive = require('massive');
 const app = express();
 app.use(express.json());
 
-const controller = require('./controller');
+const { getThisBread } = require('./controller');
 
 massive(process.env.CONNECTION_STRING)
 .then(db => {
@@ -12,15 +12,7 @@ massive(process.env.CONNECTION_STRING)
     app.set("db", db);
 })
 
-app.get('/api/students', (req, res, next) => {
-    const db = req.app.get('db');
-    db.get_all_products().then(products => {
-        res.status(200).send(products)
-        .catch(err => {
-            res.status(404).send(err)
-        });
-    });
-});
+app.get('/api/inventory', getThisBread);
 
 const port = 4000;
 app.listen(port, () => {
